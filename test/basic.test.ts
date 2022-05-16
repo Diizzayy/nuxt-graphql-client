5c5ddee5a758e5c3d7f69d416fbb3da0cee214a3
Dizzy Rogers <19627670+Diizzayy@users.noreply.github.com>
2022-05-16 11:51:03 +0000

    chore: init testing

---
+++ test/basic.test.ts
@@ -0,0 +1,15 @@
+import { fileURLToPath } from 'node:url'
+import { describe, it, expect } from 'vitest'
+import { setup, $fetch } from '@nuxt/test-utils'
+
+await setup({
+  server: true,
+  rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url))
+})
+
+describe('test suite', () => {
+  it('renders ten (10) launches', async () => {
+    const result = await $fetch('/')
+    expect(result).toContain('<p>Launch Count: 10</p>')
+  }, 15000)
+})
