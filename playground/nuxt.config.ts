9075389b52163ff84445d50dad03bde97bff2196
Dizzy Rogers <19627670+Diizzayy@users.noreply.github.com>
2022-07-18 17:00:12 +0000

    chore: use yarn workspaces

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,8 +1,7 @@
 import { defineNuxtConfig } from 'nuxt'
-import MyModule from '..'
 
 export default defineNuxtConfig({
-  modules: [MyModule],
+  modules: ['nuxt-graphql-client'],
 
   'graphql-client': { },
 
