175335e668a13d77421f45f76e37bcc71e8eafbe
Conrawl Rogers <me@diizzayy.com>
2022-10-14 16:43:57 -0400

    chore: test timeout

--- test/multi-client.test.ts
+++ test/multi-client.test.ts
@@ -11,20 +11,20 @@ describe('test multiple clients', () => {
   it('renders ten (10) launches with the spacex client', async () => {
     const result = await $fetch('/spacex')
     expect(result).toContain('<p>Launch Count: 10</p>')
-  })
+  }, 15000)
 
   it('fetches the spacex Iridium NEXT mission', async () => {
     const result = await $fetch('/spacex/mission')
     expect(result).toContain('<p>Mission Name: Iridium NEXT</p>')
-  })
+  }, 15000)
 
   it('renders the character morty', async () => {
     const result = await $fetch('/rmorty')
     expect(result).toContain('<p>Name: Morty Smith</p>')
-  })
+  }, 15000)
 
   it('fetches the name of the first rick and morty episode', async () => {
     const result = await $fetch('/rmorty/first-ep')
     expect(result).toContain('<p>First Episode: Pilot</p>')
-  })
+  }, 15000)
 })
